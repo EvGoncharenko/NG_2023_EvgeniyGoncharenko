901f1bc693d5aca584f880af41f9fd87d91c4f89
EvGoncharenko <Kilimangaro99@gmail.com>
2023-10-14 00:01:56 +0300

    Added my second task of first lection

--- Lection1/Task2/main.py
+++ Lection1/Task2/main.py
@@ -1,4 +1,4 @@
-input("Hello, this is a compatibility test for you and your friend. Click 'Enter' to get started.")
+input("Hello. Click 'Enter' to get started.")
 ageone = input("Enter your age: ")
 agetwo = input("Enter your friend's age: ")
-print("Congratulations!!Your compatibility: ", int(ageone) + int(agetwo),"%", sep="")
+print("Result of summing numbers: ", int(ageone) + int(agetwo))
