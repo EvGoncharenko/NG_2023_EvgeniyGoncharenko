67931b175f91f39cda48a9fc603328553396b162
EvGoncharenko <Kilimangaro99@gmail.com>
2023-10-22 12:43:10 +0300

    Added my fourth task of first lection

---
+++ Lection1/Task4/main.py
@@ -0,0 +1,27 @@
+import math
+sing = input("Choose a sign +,-,*,/,** or √: ")
+
+match sing:
+    case "+":
+        numone = float(input("Enter the first number: "))
+        numtwo = float(input("Enter the second number: "))
+        print("Result:", (numone) + (numtwo))
+    case "-":
+        numone = float(input("Enter the first number: "))
+        numtwo = float(input("Enter the secomd number: "))
+        print("Result:", (numone) - (numtwo))
+    case "*":
+        numone = float(input("Enter the first number: "))
+        numtwo = float(input("Enter the second number: "))
+        print("Result:", (numone) * (numtwo))
+    case "/":
+        numone = float(input("Enter first the number: "))
+        numtwo = float(input("Enter second the number: "))
+        print("Result:", "%.1f" % ((numone) / (numtwo)))
+    case "**":
+        num = float(input("Enter the number: "))
+        deg = int(input("Enter degree: "))
+        print("Result:", (num) ** (deg))
+    case "√":
+        num = int(input("Enter the number: "))
+        print("Result:", math.isqrt(num))
