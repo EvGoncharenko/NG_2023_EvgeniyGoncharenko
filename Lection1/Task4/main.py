11256223dd4051b762eec493f6c713a04fbcecf1
EvGoncharenko <Kilimangaro99@gmail.com>
2023-10-27 00:40:58 +0300

    Modified file

--- Lection1/Task4/main.py
+++ Lection1/Task4/main.py
@@ -1,27 +1,26 @@
 import math
 sing = input("Choose a sign +,-,*,/,** or √: ")
 
-match sing:
-    case "+":
-        numone = float(input("Enter the first number: "))
-        numtwo = float(input("Enter the second number: "))
-        print("Result:", (numone) + (numtwo))
-    case "-":
-        numone = float(input("Enter the first number: "))
-        numtwo = float(input("Enter the secomd number: "))
-        print("Result:", (numone) - (numtwo))
-    case "*":
-        numone = float(input("Enter the first number: "))
-        numtwo = float(input("Enter the second number: "))
-        print("Result:", (numone) * (numtwo))
-    case "/":
-        numone = float(input("Enter first the number: "))
-        numtwo = float(input("Enter second the number: "))
-        print("Result:", (numone) / (numtwo))
-    case "**":
-        num = float(input("Enter the number: "))
-        deg = int(input("Enter degree: "))
-        print("Result:", (num) ** (deg))
-    case "√":
-        num = int(input("Enter the number: "))
-        print("Result:", math.isqrt(num))
+if sing == "+" or sing == "-" or sing == "*" or sing == "/":
+    numone = float(input("Enter the first number: "))
+    numtwo = float(input("Enter the second number: "))
+    match sing:
+        case "+":
+            print("Result:", (numone) + (numtwo))
+        case "-":
+            print("Result:", (numone) - (numtwo))
+        case "*":
+            print("Result:", (numone) * (numtwo))
+        case "/":
+            print("Result:", (numone) / (numtwo))
+elif sing == "**" or sing == "√":
+    match sing:
+        case "**":
+            num = float(input("Enter the number: "))
+            deg = int(input("Enter degree: "))
+            print("Result:", (num) ** (deg))
+        case "√":
+            num = int(input("Enter the number: "))
+            print("Result:", math.isqrt(num))
+else:
+    print("This sign is missing! Choose from the above")
